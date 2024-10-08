70a99e06bc1de14ca4a12711490c793930143217
Ashar Riaz <97333911+Asharriaz14@users.noreply.github.com>
2024-10-08 12:27:11 +0500

    Update Navbar.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -7,27 +7,27 @@ import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <div>
-      <header className="flex flex-col items-center justify-center max-w-full md:max-w-6xl px-2 md:px-6 py-12 mx-auto md:flex-row">
+      <header className="flex flex-col items-center justify-center max-w-full md:max-w-6xl px-2 md:px-6 pt-12 mx-auto md:flex-row">
         <nav className="z-10">
-          <ul className="flex flex-row items-center px-6 py-2 text-indigo-100 bg-[#c2000e] rounded-full border-4   border-[#fc3e4b]">
-            <li className="p-4 mx-6">
+          <ul className="flex flex-row items-center px-6   text-indigo-100 bg-[#c2000e] rounded-full border-4   border-[#fc3e4b]">
+            <li className="p-4 ">
               <Link>
-                <FaTelegramPlane size={36} />
+                <FaTelegramPlane size={28} />
               </Link>
             </li>
             <li className="mx-6">
               <Link>
-                <FaXTwitter size={36} />
+                <FaXTwitter size={28} />
               </Link>
             </li>
             <li className="mx-6">
               <Link>
-                <GiEagleHead size={36} />
+                <GiEagleHead size={28} />
               </Link>
             </li>
             <li className="mx-6">
               <Link>
-                <SlSupport size={36} />
+                <SlSupport size={28} />
               </Link>
             </li>
           </ul>
